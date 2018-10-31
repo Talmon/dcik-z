c7a86d38c3d9cb5f541ecb4a6e801a8d80a1c733
Talmon <talmon.mwakesi@roamtech.com>
2018-10-31 17:20:56 +0300

    homepage adjustments

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -170,7 +170,7 @@
         </div>
       </li>
       <li class="nav-item dropdown" style="position: static!important;">
-        <a class="nav-link  " href="#" id="navbarDropdown" role="button" data-toggle="" aria-haspopup="true"
+        <a class="nav-link  " href="/ministries" id="navbarDropdown" role="button" data-toggle="" aria-haspopup="true"
           aria-expanded="false">
           Ministries
         </a>
